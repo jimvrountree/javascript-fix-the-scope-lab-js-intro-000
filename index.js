d79e9099a647c69a945d07bb23ba628097079aaf
Jim Rountree <jimvrountree@gmail.com>
2019-12-21 19:37:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ var animal = 'cat';
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
-}
+  return n + two }
+
+  // Feel free to move things around
+  
 
 var funkyFunction = function() {
   return function() {
